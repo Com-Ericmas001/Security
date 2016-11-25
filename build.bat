3e8f27c7aa5ee0706245fb78f783c8aa8bb66bf8
Éric Massé <Ericmas001@gmail.com>
2016-11-24 19:23:41 -0500

    travis & myget

---
+++ build.bat
@@ -0,0 +1,24 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+call %NuGet% restore Security.sln -NonInteractive
+msbuild Security.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
+
+%nuget% pack "Security.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
